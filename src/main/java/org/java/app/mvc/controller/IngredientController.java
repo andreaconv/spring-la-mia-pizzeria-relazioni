555069474bf6bfee0e339f23c5f2e717a4f572d3
andreaconv <andrea2convertini@gmail.com>
2023-10-16 10:00:28 +0200

    [ADD] form nuovo ingrediente che aggiunge l'ingrediente senza controlli

--- src/main/java/org/java/app/mvc/controller/IngredientController.java
+++ src/main/java/org/java/app/mvc/controller/IngredientController.java
@@ -7,8 +7,13 @@ import org.java.app.db.serv.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping()
@@ -17,7 +22,7 @@ public class IngredientController {
 	@Autowired
 	private IngredientService ingredientService;
 	
-	@GetMapping("/ingredient")
+	@GetMapping("/ingredients")
 	public String getIndex(Model model) {
 		
 		List<Ingredient> ingredients = ingredientService.findAll();
@@ -25,6 +30,32 @@ public class IngredientController {
 		model.addAttribute("ingredients", ingredients);
 		
 		return "ingredient-index";
+	}
+	
+	//CREATE NEW INGREDIENTE
+	@GetMapping("/ingredients/create")
+	public String getCreateForm(Model model) {
+		
+		model.addAttribute("ingredient", new Ingredient());
+		return "ingredient-create-edit";
+	}
+	@PostMapping("/ingredients/create")
+	public String storeIngredient(
+			@Valid @ModelAttribute Ingredient ingredient,
+			BindingResult bindingResult,
+			Model mode) {
+		
+		if (bindingResult.hasErrors()) {
+			System.out.println("Error:");
+			bindingResult.getAllErrors().forEach(System.out::println);
+			
+			return "ingredient-create-edit";
+		}
+		
+		ingredientService.save(ingredient);
+		
+		
+		return "redirect:/ingredients";
 	}
 
 }
